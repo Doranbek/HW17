b2783298d97bbdc20d9f868af71823efb1e4e303
Doranbek <67508373+Doranbek@users.noreply.github.com>
2020-08-28 13:22:53 +0600

    Add files via upload

---
+++ HW17-1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace HW17_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folder = "C:\\folder1";
+            var directory = new DirectoryInfo(folder);
+
+            if (!directory.Exists)
+            {
+                directory.Create();
+                directory.CreateSubdirectory(@"sub_filder1");
+                directory.CreateSubdirectory(@"sub_filder2");                
+            }
+            else
+            {
+                Console.WriteLine("Директория с именем: {0}  не существует.", directory.FullName);
+            }
+
+            string folder2 = "C:\\folder2";
+            var directory2 = new DirectoryInfo(folder2);
+            if (!directory2.Exists)
+            {
+                directory2.Create();                
+                directory.CreateSubdirectory("sub_folder2_1");
+            }
+            else
+            {
+                Console.WriteLine("Директория с именем: {0}  не существует.", directory.FullName);
+            }
+
+
+            Console.WriteLine("Директории созданы.");
+
+            Console.WriteLine($"Название каталога: {directory.Name}");
+            Console.WriteLine($"Полное название каталога: {directory.FullName}");
+            Console.WriteLine($"Время создания каталога: {directory.CreationTime}");
+            Console.WriteLine($"Корневой каталог: {directory.Root}");
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ HW17-2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace HW17_2
+{
+    class Program
+    {
+       
+
+        static async Task Main(string[] args)
+        {
+            Title();
+
+            string target = Path();
+
+            var creatFile = File.CreateText(target);
+            creatFile.Close();
+            
+            using (var targetStream = new StreamWriter(target, true))
+            {
+                string line;
+                do
+                {
+                    OutConsole("Введите слово для записи в текст\n>");
+                    line = Console.ReadLine();
+
+                    await targetStream.WriteLineAsync(line);
+                }
+                while (line != string.Empty);
+                targetStream.Close();
+            }
+            
+            Title();
+
+            OutConsole("Нажмите лубую клавишу для читение из текста\n>");
+            
+            Console.ReadKey();
+
+            using (var reader = File.OpenText(target))
+            {
+                Title();
+                Console.Write(reader.ReadToEnd());
+            }
+
+            Console.Read();
+
+            static void Title() => Console.WriteLine(new string('-', 31));
+            
+            static string Path() 
+            {
+                Console.Write("Введите путь к файлу target.txt\n>");
+                string input;
+                input = Console.ReadLine();
+                return @$"{input}\target.txt";
+            }
+            
+            static void OutConsole(string text)
+            {
+                Console.Write(text);
+            } 
+        }
+
+
+    }
+}
